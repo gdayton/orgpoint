4eec90cf771bc02a7115291b58365734d85017f1
Glenn Dayton <glenn.dayton24@gmail.com>
2016-01-19 00:41:32 -0800

    more changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create(:first_name => "Sammy", 
+				   :last_name => "Slug", 
+				   :email => "sslug@ucsc.edu", 
+				   :company_id => 1, 
+				   :password => "password", 
+				   :password_confirmation => "password")
+user.save
+
+comp = Company.create(:title => "Barbara Floor & Tiling")
+comp.save
+
+loc = Location.create(:title => "San Diego, CA", :company_id => 1)
+loc.save
+
+dep = Department.create(:title => "Management", :company_id => 1)
+dep.save
+
