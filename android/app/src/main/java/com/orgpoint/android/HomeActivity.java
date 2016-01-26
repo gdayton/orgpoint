fb1f1a4e44fac59a9d0a749a1c17121ad39988a6
josecalles <jmcallesv@gmail.com>
2016-01-26 01:43:40 -0800

    push android source

---
+++ android/app/src/main/java/com/orgpoint/android/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.orgpoint.android;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
+
+    Button button = new Button(this);
+
+    button.setOnClickListener();
+
+  }
+
+
+}
