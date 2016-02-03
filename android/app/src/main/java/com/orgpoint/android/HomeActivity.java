e068204db92dd0d19b51a066e5980c446c24331a
josecalles <jmcallesv@gmail.com>
2016-02-02 19:39:42 -0800

    Add log statement to HomeActivity

--- android/app/src/main/java/com/orgpoint/android/HomeActivity.java
+++ android/app/src/main/java/com/orgpoint/android/HomeActivity.java
@@ -3,6 +3,7 @@ package com.orgpoint.android;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
@@ -15,6 +16,8 @@ public class HomeActivity extends AppCompatActivity {
     Button button = new Button(this);
 
     button.setOnClickListener();
+
+    Log.d("LOG", "This is a test log");
 
   }
 
