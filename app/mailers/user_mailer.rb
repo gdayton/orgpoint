967e22186ee31aefde7fcc5a66b831f7079b473f
Glenn Dayton <glenn.dayton24@gmail.com>
2016-02-03 20:14:38 -0800

    trying to get the mailer to work

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
 	def registration_confirmation_onboard(user)
 		@user = user
 		mail(
-			:to => "#{user.first_name} #{user.last_name}",
+			:to => "#{@user.first_name} #{@user.last_name}",
 			:subject => "OrgPoint Registration Confirmation"
 		)
 	end
