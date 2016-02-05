d7026e04c9f707201718634ce7e510bff7b054ae
rlopezlu <rlopezlu@ucsc.edu>
2016-02-04 18:31:24 -0800

    display the second activity

--- android/app/src/main/java/com/orgpoint/android/secondActivity.java
+++ android/app/src/main/java/com/orgpoint/android/secondActivity.java
@@ -3,6 +3,8 @@ package com.orgpoint.android;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class secondActivity extends AppCompatActivity {
 
@@ -13,6 +15,12 @@ public class secondActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(HomeActivity.EXTRA_MESSAGE);
-        
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("Welcome "+message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
 }
