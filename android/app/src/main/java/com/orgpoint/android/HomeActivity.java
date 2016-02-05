bda98a07f05d7aabdf53e6a75272e8246291fc80
Joseph Ou <jou1@ucsc.edu>
2016-02-04 19:32:38 -0800

    Made a log in screen: Added a textview for a title message. Added two textviews to display username and password, added two EditText fields to input username and password. Modified the button a bit so when button is pressed, the user's inputted username and password will be displayed in the username and password textviews.

--- android/app/src/main/java/com/orgpoint/android/HomeActivity.java
+++ android/app/src/main/java/com/orgpoint/android/HomeActivity.java
@@ -5,23 +5,47 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
-
-    Button button = new Button(this);
-
-    button.setOnClickListener();
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_home);
 
 
-    Log.d("LOG", "This is a test log");
+      logInButtonPress();
+            Log.d("LOG", "This is a test log");
     Log.d("log", "I wrote this");
 
-  }
+    }
 
+    //Does stuff when the log in button is pressed
+    public void logInButtonPress(){
+        Button logInButton = (Button)findViewById(R.id.logInButton);
+        logInButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                final EditText username = (EditText) findViewById(R.id.home_Username);
+                final EditText password = (EditText) findViewById(R.id.home_Password);
+                Log.d("LOG", "BUTTONPRESS LOGGED");
+
+                //CHANGES username and password textviews to
+                // whatever is in the username and password fields
+                //Can reuse this code when we grab the actual data.
+                String temp = username.getText().toString();
+                String tempp = password.getText().toString();
+                TextView tempz = (TextView) findViewById(R.id.usernameTextView);
+                tempz.setText(temp);
+                tempz = (TextView) findViewById(R.id.passwordTextView);
+                tempz.setText(tempp);
+
+                //DO SOMETHING TO LOG THE USER IN.
+            }
+        });
+    }
 
 }
