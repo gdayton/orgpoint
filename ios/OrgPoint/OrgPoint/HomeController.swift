067a1eef4eb010952e07825753e5fce85238df7c
josecalles <jmcallesv@gmail.com>
2016-02-11 17:15:16 -0800

    setup navigation

---
+++ ios/OrgPoint/OrgPoint/HomeController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrgPoint
+//
+//  Created by Jose Calles on 2/4/16.
+//  Copyright © 2016 Jose Calles. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+
+
+}
+
