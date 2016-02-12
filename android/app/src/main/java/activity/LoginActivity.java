c17f253f97e77f4de8657565e4aba2c0480d7747
rlopezlu <rlopezlu@ucsc.edu>
2016-02-11 22:33:00 -0800

    xml for drawer

--- android/app/src/main/java/activity/LoginActivity.java
+++ android/app/src/main/java/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.orgpoint.android;
+package activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.orgpoint.android.R;
 
 
 public class LoginActivity extends AppCompatActivity {
