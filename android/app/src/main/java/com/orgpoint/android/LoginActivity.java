bc0d0d62130eb30738d05acd8c26850ebf971560
josecalles <jmcallesv@gmail.com>
2016-02-11 17:47:40 -0800

    clean up views

---
+++ android/app/src/main/java/com/orgpoint/android/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.orgpoint.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_log_in);
+    //
+    //logInButtonPress();
+
+  }
+
+
+  //Does stuff when the log in button is pressed
+  //public void logInButtonPress(){
+  //  Button logInButton = (Button)findViewById(R.id.logInButton);
+  //  logInButton.setOnClickListener(new View.OnClickListener() {
+  //    public void onClick(View view) {
+  //      final EditText username = (EditText) findViewById(R.id.home_Username);
+  //      final EditText password = (EditText) findViewById(R.id.home_Password);
+  //      Log.d("LOG", "BUTTONPRESS LOGGED");
+  //
+  //      //CHANGES username and password textviews to
+  //      // whatever is in the username and password fields
+  //      //Can reuse this code when we grab the actual data.
+  //      String temp = username.getText().toString();
+  //      String tempp = password.getText().toString();
+  //      TextView tempz = (TextView) findViewById(R.id.usernameTextView);
+  //      tempz.setText(temp);
+  //      tempz = (TextView) findViewById(R.id.passwordTextView);
+  //      tempz.setText(tempp);
+  //
+  //      //DO SOMETHING TO LOG THE USER IN.
+  //    }
+  //  });
+  }
+
+
