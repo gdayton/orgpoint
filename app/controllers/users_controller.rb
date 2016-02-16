87634d60e940dc5de993410cdb1faabc10e6b0f1
Glenn Dayton <glenn.dayton24@gmail.com>
2016-02-16 13:54:20 -0800

    added docs for Jose, and some other stuff, pretty big push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -81,6 +81,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :company_id, :image)
+      params.require(:user).permit(:first_name, :last_name, :email, :company_id, :image, :role)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214054534) do
+ActiveRecord::Schema.define(version: 20160216214423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20160214054534) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.boolean  "admin"
+    t.string   "role"
+    t.integer  "manager"
   end
 
   add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
