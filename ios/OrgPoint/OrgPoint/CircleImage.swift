ece2b8e21e9559f25ff45504ec2f7a6af7e50724
josecalles <jmcallesv@gmail.com>
2016-02-18 17:30:45 -0800

    add profile view

---
+++ ios/OrgPoint/OrgPoint/CircleImage.swift
@@ -0,0 +1,19 @@
+//
+//  CircleImage.swift
+//  OrgPoint
+//
+//  Created by Jose Calles on 2/18/16.
+//  Copyright © 2016 Jose Calles. All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIImageView {
+
+
+    override func awakeFromNib() {
+        self.layer.cornerRadius = self.layer.frame.width / 2
+        self.clipsToBounds = true
+    }
+
+}
