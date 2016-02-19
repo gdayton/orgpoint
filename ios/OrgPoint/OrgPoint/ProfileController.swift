47cb4cb5ca23a9e088a99a686e2faa50b8c22f60
josecalles <jmcallesv@gmail.com>
2016-02-18 17:42:27 -0800

    add view references

---
+++ ios/OrgPoint/OrgPoint/ProfileController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileController.swift
+//  OrgPoint
+//
+//  Created by Jose Calles on 2/18/16.
+//  Copyright © 2016 Jose Calles. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class ProfileController: UIViewController {
+
+    @IBOutlet weak var userImage : UIImageView!
+    @IBOutlet weak var role : UILabel!
+    @IBOutlet weak var location : UILabel!
+    @IBOutlet weak var aboutText : UITextView!
+    @IBOutlet weak var respText : UITextView!
+    @IBOutlet weak var userName : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+
+
+}
