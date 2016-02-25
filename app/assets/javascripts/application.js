ea1b013eacb6505e7bbfa14419c4ddc5ad2c5a38
Glenn Dayton <glenn.dayton24@gmail.com>
2016-02-25 01:15:07 -0800

    edited the show.jsonbuilder file for raul to start doing whatever he needed to do

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require angular/angular
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require angular/angular
 //= require_tree .
 
 //$(function(){ $(document).foundation(); });
